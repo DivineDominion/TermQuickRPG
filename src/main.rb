f5072739df55ee93077e425ef498bbdbbc004c9f
Christian Tietze <christian.tietze@gmail.com>
2018-08-10 20:45:29 +0200

    show message on interaction

--- src/main.rb
+++ src/main.rb
@@ -4,15 +4,21 @@
 require 'curses'
 require_relative "player.rb"
 
-DIRECTIONS = {
-  "w" => :up,
-  "s" => :down,
-  "a" => :left,
-  "d" => :right,
+DIRECTION_KEYS = {
+  ?w => :up,
+  ?s => :down,
+  ?a => :left,
+  ?d => :right,
   Curses::Key::UP => :up,
   Curses::Key::DOWN => :down,
   Curses::Key::LEFT => :left,
   Curses::Key::RIGHT => :right
+}
+
+ACTION_KEYS = {
+  ?e => :use,
+  " " => :use,
+  Curses::Key::ENTER => :use,
 }
 
 Curses.init_screen
@@ -28,6 +34,31 @@ Curses.setpos(Curses.lines - 1, (Curses.cols - TITLE.length) / 2)
 Curses.addstr(TITLE)
 Curses.refresh
 
+def show_message(*lines)
+  continue_msg = "[Press Any Key]"
+  height = lines.length + 3
+  longest_line_length = lines.sort { |a,b| a.length <=> b.length }[-1].length
+  width = [continue_msg.length, longest_line_length].max + 6
+  top, left = (Curses.lines - height) / 2, (Curses.cols - width) / 2
+  win = Curses::Window.new(height, width, top, left)
+  win.keypad(true)
+  win.box("|", "-", "+")
+  
+  y = 1
+  lines.each do |line|
+    win.setpos(y, 3)
+    win.addstr(line)
+    y += 1
+  end
+  
+  win.setpos(y, (width - continue_msg.length) / 2)
+  win.addstr(continue_msg)
+  
+  win.refresh
+  win.getch
+  win.close
+end
+
 begin
   win = Curses::Window.new(Curses.lines - 3, Curses.cols - 2, 1, 1)
   win.box(?|, ?-)
@@ -38,9 +69,9 @@ begin
   
   player = Player.new(5,5)
   player.draw(win)
-  
+    
   quit = false
-  color = 1
+
   while !quit
     win.refresh
 
@@ -49,15 +80,25 @@ begin
     case input
     when "q", Curses::Key::EXIT, Curses::Key::CANCEL, Curses::Key::BREAK
       quit = true
-      
-    when -> (c) { DIRECTIONS.keys.include?(c) }
+    
+    when -> (c) { DIRECTION_KEYS.keys.include?(c) }
+      direction = DIRECTION_KEYS[input]
       old_y, old_x = [player.y, player.x]
       
       win.setpos(old_y, old_x)
       win.addstr(" ")
 
-      player.move(DIRECTIONS[input])
+      player.move(direction)
       player.draw(win)
+    
+    when -> (c) { ACTION_KEYS.keys.include?(c) }
+      action = ACTION_KEYS[input]
+      
+      show_message("Invalid Input", "Cannot interact with anything here.")
+      win.clear
+      win.box(?|, ?-)
+      player.draw(win)
+      win.refresh
     end
   end
   
