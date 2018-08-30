7b3b4920a9b2bd6883066090d5a2e4d6450d7fbc
Christian Tietze <christian.tietze@gmail.com>
2018-08-30 19:46:00 +0200

    use thread-safe queue in RunLoop

--- lib/termquickrpg/util/run_loop.rb
+++ lib/termquickrpg/util/run_loop.rb
@@ -1,3 +1,5 @@
+require "thread"
+
 module TermQuickRPG
   module Util
     # Main game loop handler. Use RunLoop#enqueue to ensure that a block is
@@ -11,16 +13,32 @@ module TermQuickRPG
 
       private :initialize
 
-      def run
-        @enqueued_blocks.reverse.each { |b| b.call() }
-        @enqueued_blocks = []
+      def initialize
+        @next_tasks_queue = Queue.new
+      end
 
+      def run
+        process_task_queue
         yield
       end
 
       def enqueue(&block)
-        @enqueued_blocks ||= []
-        @enqueued_blocks << block
+        @next_tasks_queue << block
+      end
+
+      private
+
+      def process_task_queue
+        # Replace @next_tasks_queue so additional enqueued tasks will be processed in the next run
+        current_task_queue = @next_tasks_queue
+        @next_tasks_queue = Queue.new
+
+        until current_task_queue.empty?
+          task = current_task_queue.pop(true) rescue nil
+          if task
+            task.call()
+          end
+        end
       end
     end
   end
