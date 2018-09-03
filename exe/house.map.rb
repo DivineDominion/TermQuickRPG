f6fa92baf18d6eabb996ff42342dd34c3090f322
Christian Tietze <christian.tietze@gmail.com>
2018-09-03 21:14:28 +0200

    add house map

---
+++ exe/house.map.rb
@@ -0,0 +1,20 @@
+{
+size: [9, 6],
+solids: ["█"],
+layers: [
+  ["██████░██",
+   "█   ☻   █",
+   "█████████",
+   "█       █",
+   "█       █",
+   "███░░████"],
+],
+player_position: [4, 4],
+triggers: {
+  [3..4, 5] => -> (ctx) {
+    ctx.run do
+      leave_map
+    end
+  }
+}
+}
