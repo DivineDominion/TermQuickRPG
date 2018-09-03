f6b1cea43720de21cae01d42cdbd62e30153aeed
Christian Tietze <christian.tietze@gmail.com>
2018-09-03 21:13:35 +0200

    add Context.main singleton

--- lib/termquickrpg/script/context.rb
+++ lib/termquickrpg/script/context.rb
@@ -3,6 +3,14 @@ require "termquickrpg/script/map_commands"
 module TermQuickRPG
   module Script
     class Context
+      class << self
+        def main
+          @@main ||= Context.new()
+        end
+
+        def run(&block); main.run(&block); end
+      end
+
       def run(&block)
         instance_eval(&block)
       end
