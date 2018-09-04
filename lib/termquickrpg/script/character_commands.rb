5e71d13d36397f93dfe60f18362f17dede3ea610
Christian Tietze <christian.tietze@gmail.com>
2018-09-04 18:36:33 +0200

    use 0.0 movement delay in scripts by default

--- lib/termquickrpg/script/character_commands.rb
+++ lib/termquickrpg/script/character_commands.rb
@@ -7,8 +7,8 @@ module TermQuickRPG
         current_map.player_character
       end
 
-      def move(character, direction, after_delay = 0.5)
-        sleep after_delay
+      def move(character, direction, after_delay = 0)
+        sleep(after_delay) if after_delay > 0
         character.move(direction)
       end
     end
