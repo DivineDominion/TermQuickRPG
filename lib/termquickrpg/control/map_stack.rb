e64596f92c8393e4a12479c36f4df5adb2f3aa0a
Christian Tietze <christian.tietze@gmail.com>
2018-09-04 21:34:14 +0200

    keep maps open and reshuffle in stack

--- lib/termquickrpg/control/map_stack.rb
+++ lib/termquickrpg/control/map_stack.rb
@@ -8,30 +8,33 @@ module TermQuickRPG
       include Observable
 
       attr_reader :maps
+      attr_reader :cache
 
       def initialize(*maps)
         @maps = maps || []
+        @cache = maps.map { |map| [map.id, map] }.to_h
       end
 
       def push(map)
-        if last_map = @maps.last
-          last_map.active = false
-        end
-        @maps << map
+        return if front && front.id == map.id
+        front.active = false if front
+        map.active = true
+        maps << (cache[map.id] || map)
+        cache[map.id] = map unless cache.has_key?(map.id)
       ensure
         notify_listeners(:map_stack_did_change)
       end
 
       def pop
-        map = @maps.pop
-        @maps.last.active = true
+        map = maps.pop
+        front.active = true if front
         map
       ensure
         notify_listeners(:map_stack_did_change)
       end
 
       def front
-        @maps.last
+        maps.last
       end
     end
   end
